aad2d3fba4ee7f52ca5f1025b71427919301d639
r4i0b <taoxingyu@gmail.com>
2016-05-04 10:28:44 -0700

    imported menu system from BB3

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public void loadLevel(string name){
+        //Application.LoadLevel(name);
+        SceneManager.LoadScene(name);
+    }
+
+    public void quitRequest()
+    {
+        Application.Quit();
+    }
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+}
