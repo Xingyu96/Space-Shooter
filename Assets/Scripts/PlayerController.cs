6d75c3110178de06e5eee20d4e065be831f38466
r4i0b <taoxingyu@gmail.com>
2016-05-04 11:28:17 -0700

    clamped player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,21 +2,26 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-    public float speed = 1.0f;
+    public float speed = 15f;
 
     private Vector3 position;
+    private float xmin = -5f;
+    private float xmax = 5f;
 
 	
 	// Update is called once per frame
 	void Update () {
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            this.transform.position -= new Vector3(speed, 0, 0);
+            this.transform.position += Vector3.left * speed * Time.deltaTime;
         }
         else if (Input.GetKey(KeyCode.RightArrow))
         {
-            this.transform.position += new Vector3(speed, 0, 0);
+            this.transform.position += Vector3.right * speed * Time.deltaTime;
         }
+
+        //restrict player to gamescpace
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, xmin, xmax), transform.position.y, transform.position.z);
 
 	}
 }
