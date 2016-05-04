f9120dddbf6aabdc1e91c66c89c4a7483b810ec9
r4i0b <taoxingyu@gmail.com>
2016-05-04 11:11:55 -0700

    added sprite for player, added player controler, added arrow control to move player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+    public float speed = 1.0f;
+
+    private Vector3 position;
+
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.transform.position -= new Vector3(speed, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.transform.position += new Vector3(speed, 0, 0);
+        }
+
+	}
+}
