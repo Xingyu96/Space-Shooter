a63c9567e22e220175da0c9983c35e2a8ae756bd
r4i0b <taoxingyu@gmail.com>
2016-05-04 21:41:41 -0700

    last commit for today

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        //Debug.Log("collision");
+        Projectile laser = col.gameObject.GetComponent<Projectile>();
+        if (laser)
+        {
+            Debug.Log("hit by a laser");
+        }
+    }
+}
--- Assets/Scripts/Shredder.cs
+++ Assets/Scripts/Shredder.cs
@@ -5,6 +5,7 @@ public class Shredder : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D col)
     {
+        //Debug.Log("shred");
         Destroy(col.gameObject);
     }
 }
